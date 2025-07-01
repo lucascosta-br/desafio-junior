20c2f52cce72312ebc2840d697a68d6633975d8a
Lucas Costa <ls.coosta@hotmail.com>
2025-07-01 13:08:47 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/github/lucascostabr/desafio_junior/DesafioJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucascostabr.desafio_junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucascostabr/desafio_junior/DesafioJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucascostabr.desafio_junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
